76251a72f0bcda1825ac3b2829a8c0cecfe09b45
Fran Cirka <frandres00@gmail.com>
2021-10-15 23:15:37 +0200

    Fixed typo

--- FullCodeExamples/chapter3/fibo/src/main.rs
+++ FullCodeExamples/chapter3/fibo/src/main.rs
@@ -5,9 +5,9 @@ fn main() {
 fn fibo_calc(mut x: i32) {
     let mut z = 0; //Sum vuale
     let mut fibo = 1; // Last digit
-    let mut finalfibo = 0; // Current value in the fibonaccie series
+    let mut finalfibo = 0; // Current value in the fibonacci series
     for _i in 1..x {
-        z = fibo + finalfibo; //Summing the last digit (In 1st iter == 1) with the current == 0
+        z = fibo + finalfibo; //Adding the last digit (In 1st iter == 1) with the current == 0
         finalfibo = fibo; // Assigning the current digit to the last digit
         fibo = z; //Assigning the last digit to the sum value
     }
